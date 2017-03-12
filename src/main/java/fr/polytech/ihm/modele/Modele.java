1d4b6b56b4c32afc2a49f226fe3b8075733ba242
Aghiles <aghiles.dziri@etu.unice.fr>
2017-03-12 19:52:35 +0100

    admin : ajout de produit

--- src/main/java/fr/polytech/ihm/modele/Modele.java
+++ src/main/java/fr/polytech/ihm/modele/Modele.java
@@ -5,23 +5,18 @@ import javafx.collections.ObservableList;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 public abstract class Modele {
 	private static ObservableList<Produit> produitList = FXCollections.observableList(new ArrayList());
-	private static List<String> categories = new ArrayList<>();
 	
 	private Modele(){
 	}
 	
 	public static void init() throws IOException{
-		categories.add("Livre");
-		categories.add("CD");
-		categories.add("DVD");
-		produitList.add(new Produit("ttttttttttttttttttt","le monde" , 2,40, 9.0f, "/images/produits/nirvana.jpg","CD"));
-		produitList.add(new Produit("ttttttttttttttttttt","le monde" , 2,40, 9.0f, "/images/produits/louane.jpg","CD"));
-		produitList.add(new Produit("ttttttttttttttttttt","le monde" , 2,40, 9.0f, "/images/produits/nirvana.jpg","CD"));
-		produitList.add(new Produit("ttttttttttttttttttt","le monde" , 2,40, 9.0f, "/images/produits/louane.jpg","CD"));
+		produitList.add(new Produit("ttttttttttttttttttt","le monde" , "2","40", "9.0", "/images/produits/nirvana.jpg","CD"));
+		produitList.add(new Produit("ttttttttttttttttttt","le monde" , "2","40", "9.0", "/images/produits/louane.jpg","CD"));
+		produitList.add(new Produit("ttttttttttttttttttt","le monde" , "2","40", "9.0", "/images/produits/nirvana.jpg","CD"));
+		produitList.add(new Produit("ttttttttttttttttttt","le monde" , "2","40", "9.0", "/images/produits/louane.jpg","CD"));
 	}
 	
 	public static ObservableList<Produit> getProducts(){
