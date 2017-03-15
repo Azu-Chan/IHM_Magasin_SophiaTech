c904c6ce573fede3dbd47352bbd0d699bd74053c
Dylan Ritrovato <dylan-jonathan.ritrovato@etu.unice.fr>
2017-03-15 13:28:34 +0100

    Rajout d'images pour diapo accueil

--- src/main/java/fr/polytech/ihm/controller/AccueilController.java
+++ src/main/java/fr/polytech/ihm/controller/AccueilController.java
@@ -15,20 +15,32 @@ public class AccueilController extends MenuController{
     @FXML
     private ImageView bienvenue;
     private List<String> listeOfImage;
+    
     @FXML
     public void initialize(){
         listeOfImage = new ArrayList<>();
-        listeOfImage.add("/images/bienvenue.jpg");
-        listeOfImage.add("/images/promotion.png");
+        listeOfImage.add("/images/diapoAccueil/bienvenue.jpg");
+        listeOfImage.add("/images/diapoAccueil/promotion.png");
+        listeOfImage.add("/images/diapoAccueil/dvdp_inline.jpg");
+        listeOfImage.add("/images/diapoAccueil/sa_6.jpg");
+        bienvenue.setImage(new Image(listeOfImage.get(cpt)));
         
         if(langue)
             anglais();
 
     }
-    public void remplaceImage(){
+    
+    public void remplaceImageUp(){
         cpt++;
         if(cpt == listeOfImage.size())
             cpt = 0;
+        bienvenue.setImage(new Image(listeOfImage.get(cpt)));
+    }
+    
+    public void remplaceImageDown(){
+        cpt--;
+        if(cpt == -1)
+            cpt = listeOfImage.size() - 1;
         bienvenue.setImage(new Image(listeOfImage.get(cpt)));
     }
     
